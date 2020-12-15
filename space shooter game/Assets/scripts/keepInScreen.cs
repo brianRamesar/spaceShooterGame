5f2b713406f1acb19e65fb7da5ac8f8f08755f93
brian ramesar <bramesar5@gmail.com>
2020-12-15 12:23:11 -0500

    added first version

---
+++ space shooter game/Assets/scripts/keepInScreen.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keepInScreen : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -2.5f, 2.5f), 
+            Mathf.Clamp(transform.position.y, -4f, 4f), transform.position.z);
+    }
+}
---
+++ space shooter game/Assets/scripts/movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float speed = 1f;
+    public float rotateSpeed = 1f;
+    Rigidbody2D rb;
+
+    // Update is called once per frame
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+     
+    }
+
+    void Update()
+    {
+        float xMove = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(xMove, rb.velocity.y).normalized * speed;
+
+        bool rightPress = Input.GetKeyDown("d");
+
+    }
+
+   
+}
