45abd85a7f6ffe846a874925ace20a7da8bb1985
brian ramesar <bramesar5@gmail.com>
2020-12-18 16:47:43 -0500

    added coin spawn

--- space shooter game/Assets/scripts/deployCoin.cs
+++ space shooter game/Assets/scripts/deployCoin.cs
@@ -7,27 +7,16 @@ public class deployCoin : MonoBehaviour
 
     public GameObject coinPrefab;
     public float respawnTime = 1.0f;
-    private Vector2 screenBounds;
 
-    // Start is called before the first frame update
     void Start()
     {
-        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
-        StartCoroutine(coinWave());
+        
     }
 
-    private void spawncoin()
+
+    private void Update()
     {
-        GameObject c = Instantiate(coinPrefab) as GameObject;
-        c.transform.position = new Vector2(screenBounds.x - 2, Random.Range(-screenBounds.x, screenBounds.x));
+        Instantiate(coinPrefab, new Vector3(-2, 6, 8), Quaternion.identity);
     }
-    // Update is called once per frame
-    IEnumerator coinWave()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(respawnTime);
-            spawncoin();
-        }
-    }
+   
 }
