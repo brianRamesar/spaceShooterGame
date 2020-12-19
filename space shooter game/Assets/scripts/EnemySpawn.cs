2d81ba0575505d60655eadc3af08992a4c8ac3ea
brian ramesar <bramesar5@gmail.com>
2020-12-19 14:01:51 -0500

    added enemy spawn

---
+++ space shooter game/Assets/scripts/EnemySpawn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public GameObject coinPrefab;
+    public GameObject p1;
+    public GameObject p2;
+    float pos;
+
+    public float secondsBetSpawn;
+    public float elapsedTime = 0.0f;
+
+    void Start()
+    {
+
+    }
+
+
+    private void Update()
+    {
+        float pos = Random.Range(p1.transform.position.x, p2.transform.position.x);
+
+        elapsedTime += Time.deltaTime;
+
+        if (elapsedTime > secondsBetSpawn)
+        {
+            elapsedTime = 0;
+
+            Instantiate(coinPrefab, new Vector3(pos, 7, 8), Quaternion.identity);
+        }
+
+
+    }
+
+}
