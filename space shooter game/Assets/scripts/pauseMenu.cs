e395aef6420fc5ac95eb699cb82233daacc1315b
brian ramesar <bramesar5@gmail.com>
2020-12-23 15:02:14 -0500

    deleted health bar, added pause mechanic

---
+++ space shooter game/Assets/scripts/pauseMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseMenu : MonoBehaviour
+{
+
+    bool isPaused = false;
+
+  
+
+    public void pauseGame()
+    {
+        if (isPaused)
+        {
+            Time.timeScale = 1;
+            isPaused = false;
+
+        }
+        else
+        {
+            Time.timeScale = 0;
+            isPaused = true;
+
+        }
+    }
+
+
+
+
+}
