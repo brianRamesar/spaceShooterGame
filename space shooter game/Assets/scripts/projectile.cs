1d1922ed5687ae5259c9b1d77874f89b9bacc953
brian ramesar <bramesar5@gmail.com>
2020-12-24 14:43:23 -0500

    fiexed hit detection, and alien hit box

--- space shooter game/Assets/scripts/projectile.cs
+++ space shooter game/Assets/scripts/projectile.cs
@@ -6,19 +6,22 @@ public class projectile : MonoBehaviour
 {
     public float speed;
     public float lifeTime;
+
+    public float distance;
     private void Start()
     {
         Invoke("DestroyProjectile", lifeTime);
     }
     private void Update()
     {
-        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.up);
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.up, distance);
 
         if(hitInfo.collider != null)
         {
             if (hitInfo.collider.CompareTag("enemy"))
             {
                 Debug.Log("alien hit");
+                DestroyProjectile();
             }
         }
 
